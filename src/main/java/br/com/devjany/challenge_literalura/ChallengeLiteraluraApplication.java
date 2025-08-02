5f2e348c3e75f057fe2df17747a9544cd2f6abf7
Janyelle <janyelleoliveira50@gmail.com>
2025-08-02 16:13:35 -0300

    Adiciona arquivos base do projeto

--- src/main/java/br/com/devjany/challenge_literalura/ChallengeLiteraluraApplication.java
+++ src/main/java/br/com/devjany/challenge_literalura/ChallengeLiteraluraApplication.java
@@ -1,13 +1,26 @@
 package br.com.devjany.challenge_literalura;
 
+import br.com.devjany.challenge_literalura.Principal.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class ChallengeLiteraluraApplication {
+@EnableTransactionManagement
+public class ChallengeLiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private Principal principal;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		principal.exibeMenu();
 	}
 
 }
---
+++ src/main/java/br/com/devjany/challenge_literalura/model/Livro.java
@@ -0,0 +1,86 @@
+package br.com.devjany.challenge_literalura.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String titulo;
+
+    private String idioma;
+    private Double numeroDownloads;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "livro_autor",
+            joinColumns = @JoinColumn(name = "livro_id"),
+            inverseJoinColumns = @JoinColumn(name = "autor_id")
+    )
+    private List<Autor> autores = new ArrayList<>();
+
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void addAutores(Autor autor) {
+        this.autores.add(autor);
+        autor.getLivros().add(this);
+    }
+
+    public Double getNumeroDownloads() {
+        return numeroDownloads;
+    }
+
+    public void setNumeroDownloads(Double numeroDownloads) {
+        this.numeroDownloads = numeroDownloads;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    @Override
+    public String toString() {
+        String nomesAutores = autores.stream()
+                .map(Autor::getNome)
+                .collect(Collectors.joining(", "));
+        return "------ LIVRO ------" +
+                "\nTítulo: " + titulo +
+                "\nAutor(es): " + (nomesAutores.isEmpty() ? "Desconhecido" : nomesAutores) +
+                "\nIdioma: " + idioma +
+                "\nDownloads: " + numeroDownloads +
+                "\n-------------------";
+    }
+}
